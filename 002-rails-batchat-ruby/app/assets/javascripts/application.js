e8ec87d18a4beae540b9ffbea464058f0283ba7d
Ryan Jones <ryan.michael.jones@gmail.com>
2012-07-14 23:06:50 -0600

    setting up

--- 002-rails-batchat-ruby/app/assets/javascripts/application.js
+++ 002-rails-batchat-ruby/app/assets/javascripts/application.js
@@ -12,4 +12,23 @@
 //
 //= require jquery
 //= require jquery_ujs
+
+// Batman.js and its adapters
+//= require batman/es5-shim
+//= require batman/batman
+//= require batman/batman.jquery
+//= require batman/batman.rails
+
+//= require batchat
+
+//= require_tree ./models
+//= require_tree ./controllers
+//= require_tree ./helpers
+
+
 //= require_tree .
+// Run the Batman app
+$(document).ready(function(){
+  Batchat.run();
+});
+
